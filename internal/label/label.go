d0b089c7c97549baf22d760b7e81ad4996c18d14
rutaka <rutaka.nashimo@gmail.com>
2024-11-27 20:23:31 +0100

    do not expose private fields

--- internal/label/label.go
+++ internal/label/label.go
@@ -8,14 +8,14 @@ type Label struct {
     ID           int64 `json:"id"`
     Name         string `json:"name"`
     Comment      string `json:"comment"`
-	Templates    map[string]Template
-	Placeholders map[string]string
+	templates    map[string]Template
+	placeholders map[string]string
 }
 
 func (l Label) Print(pType string) ([]byte, error) {
-	tplt, ok := l.Templates[pType]
+	tplt, ok := l.templates[pType]
 	if !ok {
 		return nil, fmt.Errorf("label has no template with type: %s", pType)
 	}
-	return tplt.Print(l.Placeholders)
+	return tplt.Print(l.placeholders)
 }
